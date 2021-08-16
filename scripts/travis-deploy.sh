48b091f416b308a4ba3f373953a9a1f15ae36ef6
wojtekMate <wojtasiak.mateo@gmail.com>
2021-08-16 11:00:59 +0200

    fix ci/cd ui

--- scripts/travis-deploy.sh
+++ scripts/travis-deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo Deploy application on branch $TRAVIS_BRANCH to server:
-sshpass -p $ApiaryDiaryIPPassword ssh -o 'StrictHostKeyChecking=no' root@$ApiaryDiaryIPAddress "docker-compose down && docker-compose build --pull && docker-compose up -d"
+sshpass -p $ApiaryDiaryIPPassword ssh -o 'StrictHostKeyChecking=no' root@$ApiaryDiaryIPAddress "docker-compose down && docker-compose pull && docker-compose up -d"
