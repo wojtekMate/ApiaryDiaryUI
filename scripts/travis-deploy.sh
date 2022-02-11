b9485128a25edf89f95c54654d2e5f0af94c6b1f
wojtekMate <wojtasiak.mateo@gmail.com>
2022-02-11 08:28:12 +0100

    clear docker images

--- scripts/travis-deploy.sh
+++ scripts/travis-deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo Deploy application on branch $TRAVIS_BRANCH to server:
-sshpass -p $ApiaryDiaryPassword ssh -o 'StrictHostKeyChecking=no' root@$ApiaryDiaryAddress "docker-compose down && docker-compose pull && docker-compose up -d"
+sshpass -p $ApiaryDiaryPassword ssh -o 'StrictHostKeyChecking=no' root@$ApiaryDiaryAddress "docker-compose down && docker-compose pull && docker-compose up -d && docker system prune"
