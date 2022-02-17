c3af725d40a525953369c37e32b98d5227e0bc36
Mateusz Wojtasiak <42837771+wojtekMate@users.noreply.github.com>
2022-02-17 22:31:33 +0100

    Update travis-deploy.sh

--- scripts/travis-deploy.sh
+++ scripts/travis-deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo Deploy application on branch $TRAVIS_BRANCH to server:
-sshpass -p $ApiaryDiaryPassword ssh -o 'StrictHostKeyChecking=no' root@$ApiaryDiaryAddress "docker-compose down && docker-compose pull && docker-compose up -d && echo yes && docker system prune && echo yes"
+sshpass -p $ApiaryDiaryPassword ssh -o 'StrictHostKeyChecking=no' root@$ApiaryDiaryAddress "docker-compose down && docker-compose pull && docker-compose up -d && docker system prune y"
